b0a0be3f35128b8f86d751cad4d564318c1b4aee
6xd <ldtheended@163.com>
2020-10-27 23:19:05 +0800

    a0.21（完善MVVM——引入发布者-订阅者模式）

--- myvue02.js
+++ myvue02.js
@@ -8,21 +8,28 @@ class Vue extends EventTarget{
     observer(data){
         let keys = Object.keys(data);
         let _this = this;
+        let dep = new Dep(); // 实例化管理器
         keys.forEach(key=>{
             let value = data[key];
             Object.defineProperty(data,key,{
                 configurable:true,
                 enumerable:true,
                 get(){
-                    console.log("get...111");
+                    // Dep.target == watcher实例；
+                    if(Dep.target){
+                        console.log("get...111");
+                        dep.addSub(Dep.target); // 添加到订阅者队列（实现订阅者）
+                    }
                     return value;
                 },
                 set(newValue){
                     console.log("set..");
                     // key 这里传参必须是独一无二的事件名称
                     // 重新编译；二次渲染；自定义事件；（观察者模式--订阅发布模式）
-                    let event = new CustomEvent(key,{detail:newValue});
-                    _this.dispatchEvent(event);
+                    // let event = new CustomEvent(key,{detail:newValue});
+                    // _this.dispatchEvent(event);
+                    dep.notify(newValue);
+                    console.log(dep);
                     if(newValue !== value)
                         value = newValue;
                 }
@@ -49,16 +56,48 @@ class Vue extends EventTarget{
                     let $1 = RegExp.$1; // $1代表分组里的具体内容，第一个分组，$2 第二个分组
                     node.textContent = node.textContent.replace(reg,this.$options.data[$1]);
                     // 绑定事件处理函数
-                    this.addEventListener($1,e=>{
-                        let newValue = e.detail;
-                        // 匹配旧值；
-                        let oldValue = this.$options.data[$1];
-                        // reg = /oldValue/g;（这种写法是错误的，oldValue是变量，这里会把它当成字符串）
-                        reg = new RegExp(oldValue,"g"); // 全局匹配旧值
-                        node.textContent = node.textContent.replace(reg,newValue);
-                    });
+                    // this.addEventListener($1,e=>{
+                    //     let newValue = e.detail;
+                    //     // 匹配旧值；
+                    //     let oldValue = this.$options.data[$1];
+                    //     // reg = /oldValue/g;（这种写法是错误的，oldValue是变量，这里会把它当成字符串）
+                    //     reg = new RegExp(oldValue,"g"); // 全局匹配旧值
+                    //     node.textContent = node.textContent.replace(reg,newValue);
+                    // });
+                    new Watcher((newValue)=>{
+                        console.log("---",newValue);
+                    })
+
                 }
             }
         })
+    }
+}
+
+// 管理器（订阅者列表）；
+class Dep{
+    constructor(){
+        this.subs = [];  // 订阅者队列
+    }
+    addSub(sub){
+        this.subs.push(sub);
+    }
+    // 发布
+    notify(newValue){
+        this.subs.forEach(sub=>{
+            sub.update(newValue);
+        })
+    }
+}
+
+// 订阅者
+// cb 回调函数
+class Watcher{
+    constructor(cb){
+        Dep.target = this; // this指向实例化的Watcher（静态属性）
+        this.cb = cb;
+    }
+    update(newValue){
+        this.cb(newValue); // 执行回调
     }
 }
