c025a2fb5a155c382888d73fb79453ecbee2bd98
6xd <ldtheended@163.com>
2020-10-28 13:20:37 +0800

    a0.33（es6模块化——export使用）

--- 模块化/a.js
+++ 模块化/a.js
@@ -1,16 +1,20 @@
 console.log("a文件");
-let a = 10;
-
-let obj = {
-    name:"张三",
-    age:20,
-    fn:function(){
-        console.log("fn...")
-    }
-}
-
-let fn = function(){
-    console.log(1111);
-}
-
-export default fn;
+// let a = 10;
+//
+// let obj = {
+//     name:"张三",
+//     age:20,
+//     fn:function(){
+//         console.log("fn...")
+//     }
+// }
+//
+// let fn = function(){
+//     console.log(1111);
+// }
+//
+// export default fn;
+let num = 10;
+let str = "我是字符串";
+export { num }
+export { str };
