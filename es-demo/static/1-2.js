6b25dfad0495f90f99bbcdaa92416ba6c7ae18f6
6xd <ldtheended@163.com>
2020-11-19 11:14:55 +0800

    a0.65（es6定义常量）

--- es-demo/static/1-2.js
+++ es-demo/static/1-2.js
@@ -1,8 +1,11 @@
 
-Object.defineProperty(window, 'PI', {
-    value: 3.14,
-    writable: false
-})
-console.log(PI)
-PI = 5
-console.log(PI)
+// Object.defineProperty(window, 'PI', {
+//     value: 3.14,
+//     writable: false
+// })
+// console.log(PI)
+// PI = 5
+// console.log(PI)
+
+const a = 5
+a = 6
