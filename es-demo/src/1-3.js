4b6f90f8b30dc4d86cd6727c1c39ba8cca549fd6
6xd <ldtheended@163.com>
2020-11-19 13:04:49 +0800

    a0.76（复杂一些的数组解构，只有等号左边结构匹配即可。 ）

--- es-demo/src/1-3.js
+++ es-demo/src/1-3.js
@@ -1,6 +1,9 @@
-let arr = [1, 2, 3]
+// let arr = [1, 2, 3]
 // let a = arr[0]
 // let b = arr[1]
 // let c = arr[2]
-let [a, b, c] = [1, 2, 3]
-console.log(a, b, c)
+// let [a, b, c] = [1, 2, 3]
+// console.log(a, b, c)
+
+let [a, b, [c, d]] = [1, 2, [3, 4]]
+console.log(a, b, c, d)
