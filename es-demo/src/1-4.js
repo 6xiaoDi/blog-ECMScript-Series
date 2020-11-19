18ba4af91181b5a91b34183b09e6296cbc669e4b
6xd <ldtheended@163.com>
2020-11-19 23:02:41 +0800

    a0.97（for循环与foreach的差别——for…break）

--- es-demo/src/1-4.js
+++ es-demo/src/1-4.js
@@ -1,11 +1,14 @@
-let arr = [1, 2, 3]
+let arr = [1, 2, 3, 2, 4]
 
 // for
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-// }
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] == 2){
+        break;
+    }
+    console.log(arr[i])
+}
 
 // forEach
-arr.forEach(function(elem, index, array){
-    console.log(elem, index)
-})
+// arr.forEach(function(elem, index, array){
+//     console.log(elem, index)
+// })
