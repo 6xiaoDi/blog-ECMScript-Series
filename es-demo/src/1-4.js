5cf36a1dd7b8af4a2652e50472af582259efa88a
6xd <ldtheended@163.com>
2020-11-19 23:06:53 +0800

    a0.98（for循环与foreach的差别——forEach  不允许 break关键字）

--- es-demo/src/1-4.js
+++ es-demo/src/1-4.js
@@ -1,14 +1,17 @@
 let arr = [1, 2, 3, 2, 4]
 
 // for
-for (let i = 0; i < arr.length; i++) {
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] == 2){
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+
+// forEach
+arr.forEach(function(elem, index, array){
     if(arr[i] == 2){
         break;
     }
-    console.log(arr[i])
-}
-
-// forEach
-// arr.forEach(function(elem, index, array){
-//     console.log(elem, index)
-// })
+    console.log(elem, index)
+})
