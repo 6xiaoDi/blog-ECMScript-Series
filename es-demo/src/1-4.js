7c50b21696bd19602fca96b60eebff91f2648374
6xd <ldtheended@163.com>
2020-11-19 22:30:11 +0800

    a0.96（foreach遍历数组）

--- es-demo/src/1-4.js
+++ es-demo/src/1-4.js
@@ -1,6 +1,11 @@
 let arr = [1, 2, 3]
 
 // for
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
-}
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// forEach
+arr.forEach(function(elem, index, array){
+    console.log(elem, index)
+})
