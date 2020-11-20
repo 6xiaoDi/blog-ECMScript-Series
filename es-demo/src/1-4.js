c6ce110529325be7b08bf080417547bf5a2d953d
6xd <ldtheended@163.com>
2020-11-20 12:12:32 +0800

    a1.20（for…of——遍历下标及内容）

--- es-demo/src/1-4.js
+++ es-demo/src/1-4.js
@@ -110,6 +110,10 @@ let arr = [1, 2, 3, 2, 4]
 //     console.log(item)
 // }
 
-for(let item of arr.keys()){
-    console.log(item)
+// for(let item of arr.keys()){
+//     console.log(item)
+// }
+
+for(let [index, item] of arr.entries()){
+    console.log(index, item)
 }
