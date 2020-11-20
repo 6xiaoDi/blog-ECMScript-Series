419e233139c0610a73744534314578dbc59c352f
6xd <ldtheended@163.com>
2020-11-20 14:06:24 +0800

    a1.22（如何判断一个元素是否为数组？）

--- es-demo/src/1-5.js
+++ es-demo/src/1-5.js
@@ -1,9 +1,9 @@
 // DOM
-let divs = document.getElementsByTagName('div')
-console.log(divs) // HTMLCollection
-
-let divs2 = document.getElementsByClassName('xx')
-console.log(divs2) // HTMLCollection
+// let divs = document.getElementsByTagName('div')
+// console.log(divs) // HTMLCollection
+//
+// let divs2 = document.getElementsByClassName('xx')
+// console.log(divs2) // HTMLCollection
 
 let divs3 = document.querySelectorAll('.xx')
-console.log(divs3) // NodeList
+console.log(divs3 instanceof Array) // NodeList
