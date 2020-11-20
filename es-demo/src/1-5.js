30f49621e2e3f844ed117c7bf4eed814404fbd27
6xd <ldtheended@163.com>
2020-11-20 18:17:41 +0800

    a1.25（函数接收参数的arguments也是类数组）

--- es-demo/src/1-5.js
+++ es-demo/src/1-5.js
@@ -5,12 +5,18 @@
 // let divs2 = document.getElementsByClassName('xx')
 // console.log(divs2) // HTMLCollection
 
-let divs3 = document.querySelectorAll('.xx')
-console.log(divs3 instanceof Array) // NodeList
+// let divs3 = document.querySelectorAll('.xx')
+// console.log(divs3 instanceof Array) // NodeList
 // divs3.push(123)
 
 // slice
-let arr = Array.prototype.slice.call(divs3)
-console.log(arr)
-arr.push(123)
-console.log(arr)
+// let arr = Array.prototype.slice.call(divs3)
+// console.log(arr)
+// arr.push(123)
+// console.log(arr)
+
+function foo(){
+    console.log(arguments)
+    console.log(arguments instanceof Array)
+}
+foo(1, 'abc', true)
