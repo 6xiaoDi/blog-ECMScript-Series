f471b330cf809fdd92ea8d78afeed24d53ce93f5
6xd <ldtheended@163.com>
2020-11-21 15:02:00 +0800

    a1.60（扩展运算符与rest参数——扩展运算符  => 将字符串打散放入数组。）

--- es-demo/src/1-7.js
+++ es-demo/src/1-7.js
@@ -6,9 +6,13 @@
 // foo(...arr)
 // console.log(...arr)
 
-let arr1 = [1, 2, 3]
-let arr2 = [4, 5, 6]
+// let arr1 = [1, 2, 3]
+// let arr2 = [4, 5, 6]
 // Array.prototype.push.apply(arr1, arr2)
 // console.log(arr1)
-arr1.push(...arr2)
-console.log(arr1)
+// arr1.push(...arr2)
+// console.log(arr1)
+
+let str = 'asdbbc'
+var arr = [...str]
+console.log(arr)
