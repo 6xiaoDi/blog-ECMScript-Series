0e91a6ca8c04773de009488eb64636a6125be1ed
6xd <ldtheended@163.com>
2020-11-21 22:39:04 +0800

    a1.68（箭头函数——基本使用）

--- es-demo/src/1-8.js
+++ es-demo/src/1-8.js
@@ -4,8 +4,13 @@
 //     return x + y
 // }
 
-console.log(sum)
-console.log(sum(4, 5))
-let sum = function(x, y){
+// console.log(sum)
+// console.log(sum(4, 5))
+// let sum = function(x, y){
+//     return x + y
+// }
+
+let sum = (x, y) => {
     return x + y
 }
+console.log(sum(3, 4))
