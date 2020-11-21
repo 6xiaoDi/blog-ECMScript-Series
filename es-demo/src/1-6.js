0f9c9e122818d52e0d3772d6f6a72d803d5ad493
6xd <ldtheended@163.com>
2020-11-21 10:26:07 +0800

    a1.40（函数的参数——es6参数默认值）

--- es-demo/src/1-6.js
+++ es-demo/src/1-6.js
@@ -1,6 +1,12 @@
-function foo(x, y){
-    y = y || 'world'
+// function foo(x, y){
+//     y = y || 'world'
+//     console.log(x, y)
+// }
+// // foo('hello')
+// foo('hello', 0)
+
+function foo(x, y = 'world'){
     console.log(x, y)
 }
-// foo('hello')
-foo('hello', 0)
+// foo('hello', 0)
+foo('hello')
