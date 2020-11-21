4698ec1afd338dc2b0230196e6ad304ca082ab32
6xd <ldtheended@163.com>
2020-11-21 14:55:45 +0800

    a1.59（扩展运算符与rest参数——es6 => 扩展运算符，合并两个数组。）

--- es-demo/src/1-7.js
+++ es-demo/src/1-7.js
@@ -8,5 +8,7 @@
 
 let arr1 = [1, 2, 3]
 let arr2 = [4, 5, 6]
-Array.prototype.push.apply(arr1, arr2)
+// Array.prototype.push.apply(arr1, arr2)
+// console.log(arr1)
+arr1.push(...arr2)
 console.log(arr1)
