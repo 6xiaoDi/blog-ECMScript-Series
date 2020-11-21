3abf664093d44218334bed6a31f6231a4b89bf8b
6xd <ldtheended@163.com>
2020-11-21 14:53:05 +0800

    a1.58（扩展运算符与rest参数——es5语法 => 合并两个数组）

--- es-demo/src/1-7.js
+++ es-demo/src/1-7.js
@@ -1,7 +1,12 @@
 // 扩展运算符
-function foo(a, b, c) {
-    console.log(a, b, c)
-}
-let arr = [1, 2, 3]
-foo(...arr)
-console.log(...arr)
+// function foo(a, b, c) {
+//     console.log(a, b, c)
+// }
+// let arr = [1, 2, 3]
+// foo(...arr)
+// console.log(...arr)
+
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
+Array.prototype.push.apply(arr1, arr2)
+console.log(arr1)
