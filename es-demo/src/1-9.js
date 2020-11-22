02dfd4028bfe23707563946aba8d9ac8ce650c3c
6xd <ldtheended@163.com>
2020-11-22 12:44:26 +0800

    a1.81（对象的扩展——Object.is()特点）

--- es-demo/src/1-9.js
+++ es-demo/src/1-9.js
@@ -1,16 +1,20 @@
-let name = 'zhangsan'
-let age = 30
-let s = 'school'
-let obj = {
-    name,
-    age,
-    [s]: 'abcdefg',
-    // study(){
-    //     console.log(this.name + '正在学习')
-    // }
-    study:() => {
-        console.log(this.name + '正在学习')
-    }
-}
-console.log(obj)
-obj.study()
+// let name = 'zhangsan'
+// let age = 30
+// let s = 'school'
+// let obj = {
+//     name,
+//     age,
+//     [s]: 'abcdefg',
+//     // study(){
+//     //     console.log(this.name + '正在学习')
+//     // }
+//     study:() => {
+//         console.log(this.name + '正在学习')
+//     }
+// }
+// console.log(obj)
+// obj.study()
+
+console.log(Object.is(2, '2'))
+console.log(Object.is(NaN, NaN))
+console.log(Object.is(+0, -0))
