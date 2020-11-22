2d6e2350eccd18369f362303c85c84b282e2bcfc
6xd <ldtheended@163.com>
2020-11-22 21:57:02 +0800

    a2.05（ES5中的类与继承——静态方法）

--- es-demo/src/1-11.js
+++ es-demo/src/1-11.js
@@ -1,22 +1,26 @@
 // 类
 // 构造函数
-function People(name, age) {
-    console.log(this)
-    // 实例属性
-    this.name = name
-    this.age = age
-}
+// function People(name, age) {
+//     console.log(this)
+//     // 实例属性
+//     this.name = name
+//     this.age = age
+// }
 
 // 实例方法
-People.prototype.showName = function () {
-    console.log('我的名字是' + this.name)
-}
+// People.prototype.showName = function () {
+//     console.log('我的名字是' + this.name)
+// }
 
 // 实例化对象
-let p1 = new People('张三', 24)
-console.log(p1)
-p1.showName()
+// let p1 = new People('张三', 24)
+// console.log(p1)
+// p1.showName()
 
-let p2 = new People('李四', 20)
-console.log(p2)
-p2.showName()
+// let p2 = new People('李四', 20)
+// console.log(p2)
+// p2.showName()
+
+// 静态方法
+console.log(Math.max(4, 5))
+console.log(Math.random())
