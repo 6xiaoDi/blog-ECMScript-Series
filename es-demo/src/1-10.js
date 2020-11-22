ddc5b424e09ddd961d274146d5b7801dd658f681
6xd <ldtheended@163.com>
2020-11-22 16:54:40 +0800

    a1.96（深拷贝与浅拷贝——简单示例）

--- es-demo/src/1-10.js
+++ es-demo/src/1-10.js
@@ -1,21 +1,36 @@
-let target = {
-    a: {
-        b: {
-            c: 1
-        },
-        e: 4,
-        f: 5,
-        g: 6
-    }
+// let target = {
+//     a: {
+//         b: {
+//             c: 1
+//         },
+//         e: 4,
+//         f: 5,
+//         g: 6
+//     }
+// }
+// let source = {
+//     a: {
+//         b: {
+//             c: 1
+//         },
+//         e: 2,
+//         f: 3
+//     }
+// }
+// Object.assign(target, source)
+// console.log(target)
+
+// 深拷贝
+let a = 5
+let b = a
+a = 6
+console.log(a, b)
+// 浅拷贝
+let obj1 = {
+    name: 'lisi',
+    age: 30
 }
-let source = {
-    a: {
-        b: {
-            c: 1
-        },
-        e: 2,
-        f: 3
-    }
-}
-Object.assign(target, source)
-console.log(target)
+let obj2 = obj1
+obj1.age = 18
+console.log(obj1)
+console.log(obj2)
