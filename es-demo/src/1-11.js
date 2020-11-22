1b9aa017f6c87ed2e599c19666fa6911fede121d
6xd <ldtheended@163.com>
2020-11-22 23:18:59 +0800

    a2.06（ES5中的类与继承——静态属性、方法特点）

--- es-demo/src/1-11.js
+++ es-demo/src/1-11.js
@@ -1,26 +1,41 @@
 // 类
 // 构造函数
-// function People(name, age) {
-//     console.log(this)
-//     // 实例属性
-//     this.name = name
-//     this.age = age
-// }
+function People(name, age) {
+    console.log(this)
+    // 实例属性
+    this.name = name
+    this.age = age
+    // 静态属性
+    People.count++
+}
+
+// 静态属性
+People.count = 0
+// 静态方法
+People.getCount = function(){
+    console.log(this)
+    console.log(this.age) // undefined
+    console.log('当前共有' + People.count + '个人')
+}
 
 // 实例方法
-// People.prototype.showName = function () {
-//     console.log('我的名字是' + this.name)
-// }
+People.prototype.showName = function () {
+    console.log('我的名字是' + this.name)
+}
 
 // 实例化对象
-// let p1 = new People('张三', 24)
-// console.log(p1)
-// p1.showName()
+let p1 = new People('张三', 24)
+console.log(p1)
+p1.showName()
 
-// let p2 = new People('李四', 20)
-// console.log(p2)
-// p2.showName()
+let p2 = new People('李四', 20)
+console.log(p2)
+p2.showName()
+
+// 查看实例化次数
+console.log(People.count)
+People.getCount()
 
 // 静态方法
-console.log(Math.max(4, 5))
-console.log(Math.random())
+// console.log(Math.max(4, 5))
+// console.log(Math.random())
