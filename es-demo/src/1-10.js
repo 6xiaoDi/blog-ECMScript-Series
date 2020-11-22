f558fb02590fd893a4459aa8ac65ae3123a97370
6xd <ldtheended@163.com>
2020-11-22 17:31:18 +0800

    a1.97（深拷贝与浅拷贝——JSON.parse 和 JSON.stringify 实现深拷贝）

--- es-demo/src/1-10.js
+++ es-demo/src/1-10.js
@@ -21,16 +21,34 @@
 // console.log(target)
 
 // 深拷贝
-let a = 5
-let b = a
-a = 6
-console.log(a, b)
+// let a = 5
+// let b = a
+// a = 6
+// console.log(a, b)
 // 浅拷贝
+// let obj1 = {
+//     name: 'lisi',
+//     age: 30
+// }
+// let obj2 = obj1
+// obj1.age = 18
+// console.log(obj1)
+// console.log(obj2)
+
+//JSON => '{"a": "hello", "b": "world"}'
+let obj = JSON.parse('{"a": "hello", "b": "world"}')
+console.log(obj)
+let str1 = JSON.stringify(obj)
+console.log(str1)
+
 let obj1 = {
     name: 'lisi',
     age: 30
 }
-let obj2 = obj1
+
+// 深拷贝
+let str2 = JSON.stringify(obj1)
+let obj2 = JSON.parse(str2)
 obj1.age = 18
-console.log(obj1)
 console.log(obj2)
+console.log(obj1)
