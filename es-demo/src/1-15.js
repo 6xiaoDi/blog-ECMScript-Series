d0c3c20642e56db388562393db03824aad110b25
6xd <ldtheended@163.com>
2020-11-23 21:42:49 +0800

    a2.37（新的数据结构Set——数组合并去重 set->array）

--- es-demo/src/1-15.js
+++ es-demo/src/1-15.js
@@ -32,6 +32,15 @@
 // }
 
 // 数组去重
-let arr = [1, 2, 3, 4, 2, 3]
-let s = new Set(arr)
+// let arr = [1, 2, 3, 4, 2, 3]
+// let s = new Set(arr)
+// console.log(s)
+
+// 合并去重 set->array
+let arr1 = [1, 2, 3, 4]
+let arr2 = [2, 3, 4, 5, 6]
+let s = new Set([...arr1, ...arr2])
 console.log(s)
+// Set转为数组（两种方法）
+console.log([...s])
+console.log(Array.from(s))
