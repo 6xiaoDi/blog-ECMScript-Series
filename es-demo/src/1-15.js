33c343b38e1a58e31d7dd99675765ac45e19f58c
6xd <ldtheended@163.com>
2020-11-23 21:04:39 +0800

    a2.34（新的数据结构Set——基本使用）

--- es-demo/src/1-15.js
+++ es-demo/src/1-15.js
@@ -1,3 +1,15 @@
 // 唯一的
 let s = new Set([1, 2, 3, 2])
+// 添加数据
+s.add('abc').add('def')
+// 删除数据
+s.delete(2)
+// 判断是否有某个值
+console.log(s.has('abc'))
 console.log(s)
+// 长度
+console.log(s.size)
+// 清空数据
+s.clear()
+console.log(s)
+console.log(s.size)
