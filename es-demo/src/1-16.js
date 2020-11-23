12b171b0dd787003f187a219c4372fb45f615517
6xd <ldtheended@163.com>
2020-11-24 01:07:15 +0800

    a2.42（新的数据结构Map——数组创建）

--- es-demo/src/1-16.js
+++ es-demo/src/1-16.js
@@ -1,10 +1,16 @@
-let m = new Map()
-let obj = {
-    name: 'lisi'
-}
-m.set(obj, 'es')
-console.log(m)
-console.log(m.get(obj))
-console.log(m.has(obj))
-m.delete(obj)
-console.log(m)
+// let m = new Map()
+// let obj = {
+//     name: 'lisi'
+// }
+// m.set(obj, 'es')
+// console.log(m)
+// console.log(m.get(obj))
+// console.log(m.has(obj))
+// m.delete(obj)
+// console.log(m)
+
+let map = new Map([
+    ['name', 'lisi'],
+    ['age', 5]
+])
+console.log(map)
