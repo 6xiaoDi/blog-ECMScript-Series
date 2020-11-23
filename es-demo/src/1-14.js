d882e8957db7e12f72c91585dfbbd24c56145a6a
6xd <ldtheended@163.com>
2020-11-23 11:54:03 +0800

    a2.16（新的原始数据类型Symbol——描述信息是对象）

--- es-demo/src/1-14.js
+++ es-demo/src/1-14.js
@@ -4,8 +4,14 @@
 // console.log(s2)
 // console.log(s1 === s2) // false
 
-let s1 = Symbol('foo')
-let s2 = Symbol('bar')
-console.log(s1)
-console.log(s2)
-console.log(s1 === s2)// false
+// let s1 = Symbol('foo')
+// let s2 = Symbol('bar')
+// console.log(s1)
+// console.log(s2)
+// console.log(s1 === s2)// false
+
+const obj = {
+    name: 'lisi'
+}
+let s = Symbol(obj)
+console.log(s)
