1814a0645e922b864f1cb34226e68f404d9f8b5d
6xd <ldtheended@163.com>
2020-11-23 21:28:00 +0800

    a2.36（新的数据结构Set——数组去重）

--- es-demo/src/1-15.js
+++ es-demo/src/1-15.js
@@ -1,7 +1,7 @@
 // 唯一的
-let s = new Set([1, 2, 3, 2])
+// let s = new Set([1, 2, 3, 2])
 // 添加数据
-s.add('abc').add('def')
+// s.add('abc').add('def')
 // 删除数据
 // s.delete(2)
 // 判断是否有某个值
@@ -15,18 +15,23 @@ s.add('abc').add('def')
 // console.log(s.size)
 
 // 遍历
-s.forEach(item => console.log(item))
+// s.forEach(item => console.log(item))
+//
+// for (let item of s) {
+//     console.log(item)
+// }
+//
+// for (let item of s.keys()) {
+//     console.log(item)
+// }
+// for (let item of s.values()) {
+//     console.log(item)
+// }
+// for (let item of s.entries()) {
+//     console.log(item[0], item[1])
+// }
 
-for (let item of s) {
-    console.log(item)
-}
-
-for (let item of s.keys()) {
-    console.log(item)
-}
-for (let item of s.values()) {
-    console.log(item)
-}
-for (let item of s.entries()) {
-    console.log(item[0], item[1])
-}
+// 数组去重
+let arr = [1, 2, 3, 4, 2, 3]
+let s = new Set(arr)
+console.log(s)
