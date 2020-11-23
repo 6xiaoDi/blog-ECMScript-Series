62d3ae93175e520bc4cb2211564b02d766347cc7
6xd <ldtheended@163.com>
2020-11-24 01:20:10 +0800

    a2.44（新的数据结构Map——遍历）

--- es-demo/src/1-16.js
+++ es-demo/src/1-16.js
@@ -13,9 +13,27 @@ let map = new Map([
     ['name', 'lisi'],
     ['age', 5]
 ])
-console.log(map.size)
-console.log(map.has('name'))
-console.log(map.get('age'))
-map.set('name', 'zhangsan')
-console.log(map)
+// console.log(map.size)
+// console.log(map.has('name'))
+// console.log(map.get('age'))
+// map.set('name', 'zhangsan')
+// console.log(map)
 
+// 遍历
+map.forEach((value, key) => console.log(value, key))
+
+for(let [key, value] of map){
+    console.log(key, value)
+}
+
+for(let key of map.keys()){
+    console.log(key)
+}
+
+for(let value of map.values()){
+    console.log(value)
+}
+
+for(let [key, value] of map.entries()){
+    console.log(key, value)
+}
