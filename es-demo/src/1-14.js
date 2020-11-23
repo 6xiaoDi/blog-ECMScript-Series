2031e856d7e7b645fa4e82da2e5707629c01d6d7
6xd <ldtheended@163.com>
2020-11-23 11:51:08 +0800

    a2.15（新的原始数据类型Symbol——描述信息）

--- es-demo/src/1-14.js
+++ es-demo/src/1-14.js
@@ -1,5 +1,11 @@
-let s1 = Symbol()
-let s2 = Symbol()
+// let s1 = Symbol()
+// let s2 = Symbol()
+// console.log(s1)
+// console.log(s2)
+// console.log(s1 === s2) // false
+
+let s1 = Symbol('foo')
+let s2 = Symbol('bar')
 console.log(s1)
 console.log(s2)
-console.log(s1 === s2) // false
+console.log(s1 === s2)// false
