479512af30a00eb21ac036350f4c0f33b4d07c5b
6xd <ldtheended@163.com>
2020-11-23 22:31:05 +0800

    a2.38（新的数据结构Set——求两个数组的差集）

--- es-demo/src/1-15.js
+++ es-demo/src/1-15.js
@@ -39,8 +39,21 @@
 // 合并去重 set->array
 let arr1 = [1, 2, 3, 4]
 let arr2 = [2, 3, 4, 5, 6]
-let s = new Set([...arr1, ...arr2])
-console.log(s)
+// let s = new Set([...arr1, ...arr2])
+// console.log(s)
 // Set转为数组（两种方法）
-console.log([...s])
-console.log(Array.from(s))
+// console.log([...s])
+// console.log(Array.from(s))
+
+// 交集
+let s1 = new Set(arr1)
+let s2 = new Set(arr2)
+// let result = new Set(arr1.filter(item => s2.has(item)))
+// console.log(Array.from(result))
+
+// 差集
+let arr3 = new Set(arr1.filter(item => !s2.has(item)))
+let arr4 = new Set(arr2.filter(item => !s1.has(item)))
+console.log(arr3)
+console.log(arr4)
+console.log([...arr3, ...arr4])
