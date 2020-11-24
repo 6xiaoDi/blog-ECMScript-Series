d67a3917af8c92b90d3e68cd4e7882bc119d7ef7
6xd <ldtheended@163.com>
2020-11-24 15:33:03 +0800

    a2.58（正则表达式的扩展——u修饰符(es6)  => unicode）

--- es-demo/src/1-18.js
+++ es-demo/src/1-18.js
@@ -1,14 +1,18 @@
-const str = 'aaa_aa_a'
-const reg1 = /a+/g // 每次匹配剩余的
-const reg2 = /a+/y // 剩余的第一个开始匹配
+// const str = 'aaa_aa_a'
+// const reg1 = /a+/g // 每次匹配剩余的
+// const reg2 = /a+/y // 剩余的第一个开始匹配
 
-console.log(reg1.exec(str)) // aaa
-console.log(reg2.exec(str)) // aaa
+// console.log(reg1.exec(str)) // aaa
+// console.log(reg2.exec(str)) // aaa
 
-console.log(reg1.exec(str)) // aa
+// console.log(reg1.exec(str)) // aa
 // _aa_a开始匹配
-console.log(reg2.exec(str)) // null
+// console.log(reg2.exec(str)) // null
 
-console.log(reg1.exec(str)) // a
+// console.log(reg1.exec(str)) // a
 // _aa_a找不到开头匹配a，则再从开头开始找了
-console.log(reg2.exec(str)) // aaa
+// console.log(reg2.exec(str)) // aaa
+
+const str = '\uD842\uDFB7' // 表示一个字符
+console.log(/^\uD842/.test(str)) // es5 true
+console.log(/^\uD842/u.test(str)) // es6 false
