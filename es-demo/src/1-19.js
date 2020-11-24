5010d2e797ac13fe82cf9d3e465bd67116ebbb4d
6xd <ldtheended@163.com>
2020-11-24 22:39:41 +0800

    a2.63（数值的扩展——ES6： `0B`  =>  二进制  `0O`  => 八进制）

--- es-demo/src/1-19.js
+++ es-demo/src/1-19.js
@@ -3,6 +3,13 @@
 // console.log(a.toString(2))
 
 // 二进制 -> 十进制
-const b = 101
-console.log(parseInt(5.5)) // 转换为整数
-console.log(parseInt(b, 2)) // 还有第二个参数，代表转为几进制
+// const b = 101
+// console.log(parseInt(5.5)) // 转换为整数
+// console.log(parseInt(b, 2)) // 还有第二个参数，代表转为几进制
+
+// ES6 0B二进制  0O八进制
+const a = 0B0101
+console.log(a)
+
+const b = 0O777
+console.log(b)
