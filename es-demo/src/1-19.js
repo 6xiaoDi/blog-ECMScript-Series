8551aa53f2b177f61daa7f055a113f65caf06f53
6xd <ldtheended@163.com>
2020-11-24 22:33:27 +0800

    a2.62（数值的扩展——es5:  二进制  =>  十进制）

--- es-demo/src/1-19.js
+++ es-demo/src/1-19.js
@@ -1,3 +1,8 @@
 // 十进制 -> 二进制
-const a = 5 // 101
-console.log(a.toString(2))
+// const a = 5 // 101
+// console.log(a.toString(2))
+
+// 二进制 -> 十进制
+const b = 101
+console.log(parseInt(5.5)) // 转换为整数
+console.log(parseInt(b, 2)) // 还有第二个参数，代表转为几进制
