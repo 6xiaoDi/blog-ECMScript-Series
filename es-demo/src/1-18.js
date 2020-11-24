2a7a67e755dc2e9cc819c4300041214b1953b854
6xd <ldtheended@163.com>
2020-11-24 15:21:33 +0800

    a2.57（正则表达式的扩展——`/y`  =>  剩余的第一个开始匹配）

--- es-demo/src/1-18.js
+++ es-demo/src/1-18.js
@@ -1,6 +1,14 @@
 const str = 'aaa_aa_a'
 const reg1 = /a+/g // 每次匹配剩余的
+const reg2 = /a+/y // 剩余的第一个开始匹配
 
 console.log(reg1.exec(str)) // aaa
+console.log(reg2.exec(str)) // aaa
+
 console.log(reg1.exec(str)) // aa
+// _aa_a开始匹配
+console.log(reg2.exec(str)) // null
+
 console.log(reg1.exec(str)) // a
+// _aa_a找不到开头匹配a，则再从开头开始找了
+console.log(reg2.exec(str)) // aaa
