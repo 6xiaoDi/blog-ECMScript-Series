2002ced547663b5d2728d5862389ee3da089725e
6xd <ldtheended@163.com>
2020-11-24 13:49:54 +0800

    a2.50（字符串的扩展——模版字符串的使用）

--- es-demo/src/1-17.js
+++ es-demo/src/1-17.js
@@ -7,9 +7,29 @@ const str1 = '阿斯顿发斯\n'
 + '师按时发顺丰'
 console.log(str1);
 
+const str3 = `阿斯顿发斯
+蒂芬阿萨德法师法
+师按时发顺丰`
+console.log(str3);
+
+const str4 = `
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+`
+console.log(str4)
+
+
 const a = 20
 const b = 14
 const c = '李四'
 const str2 = '我的年龄是:' + (a + b) + ',我是' + c
 console.log(str2)
+
+const str5 = `我的年龄是:${a+b},我是${c}`
+console.log(str5)
+
+
 
