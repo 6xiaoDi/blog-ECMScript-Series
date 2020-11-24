41998b3c9df1da266f5e623256eee203cf0baa9c
6xd <ldtheended@163.com>
2020-11-24 13:33:33 +0800

    a2.49（字符串的扩展——es5常规字符串拼接 ）

--- es-demo/src/1-17.js
+++ es-demo/src/1-17.js
@@ -1,3 +1,15 @@
-for(let item of 'abc123'){
-    console.log(item)
-}
+// for(let item of 'abc123'){
+//     console.log(item)
+// }
+
+const str1 = '阿斯顿发斯\n'
++ '蒂芬阿萨德法师法\n'
++ '师按时发顺丰'
+console.log(str1);
+
+const a = 20
+const b = 14
+const c = '李四'
+const str2 = '我的年龄是:' + (a + b) + ',我是' + c
+console.log(str2)
+
