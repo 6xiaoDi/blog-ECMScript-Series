6fd113862486c65732789377a46ef88f685542eb
6xd <ldtheended@163.com>
2020-11-25 20:15:45 +0800

    a2.88（反射Reflect——Reflect对象的方法与Proxy对象的方法一一对应（两者相辅相成））

--- es-demo/src/1-21.js
+++ es-demo/src/1-21.js
@@ -13,5 +13,84 @@
 // obj.name = 'es'
 // console.log(obj.name)
 
-console.log('assign' in Object) // true
-console.log(Reflect.has(Object, 'assign'))// true
+// console.log('assign' in Object) // true
+// console.log(Reflect.has(Object, 'assign'))// true
+
+let user = {
+    name: 'lisi',
+    age: 30,
+    _password: '***'
+}
+user = new Proxy(user, {
+    get(target, prop) {
+        if (prop.startsWith('_')) {
+            throw new Error('不可访问')
+        } else {
+            // return target[prop]
+            return Reflect.get(target, prop)
+        }
+    },
+    set(target, prop, val) {
+        if (prop.startsWith('_')) {
+            throw new Error('不可访问')
+        } else {
+            // target[prop] = val
+            Reflect.set(target, prop, val)
+            return true
+        }
+    },
+    deleteProperty(target, prop) { // 拦截删除
+        if (prop.startsWith('_')) {
+            throw new Error('不可删除')
+        } else {
+            // delete target[prop]
+            Reflect.deleteProperty(target, prop)
+            return true
+        }
+    },
+    ownKeys(target) {
+        // return Object.keys(target).filter(key => !key.startsWith('_'))
+        return Reflect.ownKeys(target).filter(key => !key.startsWith('_'))
+    }
+})
+
+console.log(user.age)
+try {
+    console.log(user._password)
+} catch (e) {
+    console.log(e.message)
+}
+
+user.age = 18
+console.log(user.age)
+try {
+    user._password = 'xxx'
+} catch (e) {
+    console.log(e.message)
+}
+
+for (let key in user) {
+    console.log(key)
+}
+
+delete user.age
+console.log(user.age)
+
+// apply
+let sum = (...args) => {
+    let num = 0
+    args.forEach(item => {
+        num += item
+    })
+    return num
+}
+
+sum = new Proxy(sum, {
+    apply(target, ctx, args) {
+        // return target(...args) * 2
+        return Reflect.apply(target, target, [...args]) * 2
+    }
+})
+console.log(sum(1, 2))
+console.log(sum.call(null, 1, 2, 3))
+console.log(sum.apply(null, [1, 2, 3]))
