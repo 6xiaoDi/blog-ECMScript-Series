4972ae235ca45982e768c18ac8ba7a0f6f5fabb0
6xd <ldtheended@163.com>
2020-11-25 21:29:17 +0800

    a2.90（异步操作必备知识——常考面试题）

--- es-demo/src/1-22.js
+++ es-demo/src/1-22.js
@@ -1,8 +1,15 @@
-const a = 2
-const b = 3
-console.log("同步任务",a + b) // 同步任务
+// const a = 2
+// const b = 3
+// console.log("同步任务",a + b) // 同步任务
 // 异步任务
+// setTimeout(() => {
+//     console.log(a + b)
+// }, 1000)
+// console.log('异步任务')
+
+// 1 3 2
+console.log(1)
 setTimeout(() => {
-    console.log(a + b)
-}, 1000)
-console.log('异步任务')
+    console.log(2)
+}, 0)
+console.log(3)
