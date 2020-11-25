c6e827297a193672bfdd45f5669f36f0d2ae6166
6xd <ldtheended@163.com>
2020-11-25 18:53:11 +0800

    a2.87（反射Reflect——让Object操作变成函数行为）

--- es-demo/src/1-21.js
+++ es-demo/src/1-21.js
@@ -1,14 +1,17 @@
 // Object -> Reflect
-let obj = {}
-let newVal = ''
-Reflect.defineProperty(obj, 'name', {
-    get() {
-        return newVal
-    },
-    set(val) {
-        console.log('set')
-        newVal = val
-    }
-})
-obj.name = 'es'
-console.log(obj.name)
+// let obj = {}
+// let newVal = ''
+// Reflect.defineProperty(obj, 'name', {
+//     get() {
+//         return newVal
+//     },
+//     set(val) {
+//         console.log('set')
+//         newVal = val
+//     }
+// })
+// obj.name = 'es'
+// console.log(obj.name)
+
+console.log('assign' in Object) // true
+console.log(Reflect.has(Object, 'assign'))// true
