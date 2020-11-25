51b4cb7e7f01ce5df09564936e39c7be99031e4d
6xd <ldtheended@163.com>
2020-11-25 09:47:57 +0800

    a2.70（数值的扩展——Math.trunc）

--- es-demo/src/1-19.js
+++ es-demo/src/1-19.js
@@ -36,10 +36,22 @@
 // console.log(0.10000000000000001)
 // console.log(0.10000000000000001 === 0.1) // true
 
-const max = Math.pow(2, 53) // 2的53次方
-console.log(max) // 9007199254740992
-console.log(max + 1) // 9007199254740992
-console.log(Number.MAX_SAFE_INTEGER === max - 1) // (9007199254740991) true
-console.log(Number.MIN_SAFE_INTEGER) // -9007199254740991
-console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)) // true
-console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)) // false （超出范围，即不安全了）
+// const max = Math.pow(2, 53) // 2的53次方
+// console.log(max) // 9007199254740992
+// console.log(max + 1) // 9007199254740992
+// console.log(Number.MAX_SAFE_INTEGER === max - 1) // (9007199254740991) true
+// console.log(Number.MIN_SAFE_INTEGER) // -9007199254740991
+// console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)) // true
+// console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1)) // false （超出范围，即不安全了）
+
+console.log(Math.trunc(5.5)) // 5
+console.log(Math.trunc(-5.5)) // -5
+console.log(Math.trunc(true)) // 1
+console.log(Math.trunc(false)) // 0
+console.log(Math.trunc(NaN)) // NaN
+console.log(Math.trunc(undefined)) // NaN
+console.log(Math.trunc()) // NaN
+
+console.log(Number.parseInt(5.5)) // 5
+console.log(Number.parseInt(-5.5)) // -5
+console.log(Number.parseInt(true)) // NaN
