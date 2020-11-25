24d7da10b09587f876f4eaebcaf4e19599f97276
6xd <ldtheended@163.com>
2020-11-25 23:03:24 +0800

    a2.91（Ajax原理与Callback Hell——Ajax原理  =>   原生js代码实现）

--- es-demo/src/1-22.js
+++ es-demo/src/1-22.js
@@ -8,8 +8,36 @@
 // console.log('异步任务')
 
 // 1 3 2
-console.log(1)
-setTimeout(() => {
-    console.log(2)
-}, 0)
-console.log(3)
+// console.log(1)
+// setTimeout(() => {
+//     console.log(2)
+// }, 0)
+// console.log(3)
+
+// Ajax的原理
+function ajax(url, callback) {
+    // 1、创建XMLHttpRequest对象
+    var xmlhttp
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest() // IE7之后的版本
+    } else { // 兼容早期浏览器
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+    // 2、发送请求
+    xmlhttp.open('GET', url, true)
+    xmlhttp.send()
+    // 3、服务端响应
+    xmlhttp.onreadystatechange = function () {
+        // 正确返回
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var obj = JSON.parse(xmlhttp.responseText)
+            // console.log(obj)
+            callback(obj)
+        }
+    }
+}
+
+// var url = 'http://abcxsdsa.com'
+// ajax(url, res => {
+//     console.log(res)
+// })
