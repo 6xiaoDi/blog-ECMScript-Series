6ae19e77b4ca1a6a36f6a056e7ea81db3e91148d
6xd <ldtheended@163.com>
2020-11-25 10:42:35 +0800

    a2.74（代理Proxy——使用）

--- es-demo/src/1-20.js
+++ es-demo/src/1-20.js
@@ -1,16 +1,25 @@
 // ES5
+// let obj = {}
+// let newVal = '123456'
+// Object.defineProperty(obj, 'name', {
+//     get(){
+//         console.log('get')
+//         return newVal
+//     },
+//     set(val){
+//         console.log('set')
+//         // this.name = val 会造成死递归，内存溢出
+//         newVal = val
+//     }
+// })
+// obj.name = 'es'
+// console.log(obj.name)
+
+// proxy
 let obj = {}
-let newVal = '123456'
-Object.defineProperty(obj, 'name', {
-    get(){
-        console.log('get')
-        return newVal
-    },
-    set(val){
-        console.log('set')
-        // this.name = val 会造成死递归，内存溢出
-        newVal = val
-    }
-})
-obj.name = 'es'
+let p = new Proxy(obj, {})
+p.name = 'lisi'
 console.log(obj.name)
+for(let key in obj){
+    console.log(key)
+}
