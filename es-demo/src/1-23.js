8c5751fdb7e2bff6f6e9616bea6717f3f169e60d
6xd <ldtheended@163.com>
2020-11-26 11:49:04 +0800

    a2.99（异步编程解决方案Promise——Promise改造Ajax的回调地狱请求）

--- es-demo/src/1-23.js
+++ es-demo/src/1-23.js
@@ -56,12 +56,60 @@
 //     console.log(err)
 // })
 
-let p = new Promise((resolve, reject) => {
-    reject(2)
-    resolve(1)
-})
-p.then(res => {
-    console.log(res)
-}).catch(err => {
-    console.log(err)
+// let p = new Promise((resolve, reject) => {
+//     reject(2)
+//     resolve(1)
+// })
+// p.then(res => {
+//     console.log(res)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+function ajax(url, successCallback, failCallback) {
+    // 1、创建XMLHttpRequest对象
+    var xmlhttp
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else { // 兼容早期浏览器
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+    // 2、发送请求
+    xmlhttp.open('GET', url, true)
+    xmlhttp.send()
+    // 3、服务端响应
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var obj = JSON.parse(xmlhttp.responseText)
+            // console.log(obj)
+            successCallback && successCallback(obj)
+        } else if (xmlhttp.readyState === 4 && xmlhttp.status === 404) {
+            failCallback && failCallback(xmlhttp.statusText)
+        }
+    }
+}
+
+new Promise((resolve, reject) => {
+    ajax('static/a.json', res => {
+        console.log(res)
+        resolve()
+    })
+}).then(res => {
+    console.log('a成功')
+    return new Promise((resolve, reject) => {
+        ajax('static/b.json', res => {
+            console.log(res)
+            resolve()
+        })
+    })
+}).then(res => {
+    console.log('b成功')
+    return new Promise((resolve, reject) => {
+        ajax('static/c.json', res => {
+            console.log(res)
+            resolve()
+        })
+    })
+}).then(res => {
+    console.log('c成功')
 })
