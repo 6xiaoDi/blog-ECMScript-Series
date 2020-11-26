d0334207ee81285f37e92aa48ec660b3691291f3
6xd <ldtheended@163.com>
2020-11-26 22:59:29 +0800

    a3.16（异步编程解决方案Generator——next没有参数的情况）

--- es-demo/src/1-25.js
+++ es-demo/src/1-25.js
@@ -10,8 +10,18 @@
 // console.log(f.next())
 // console.log(f.next())
 
-function* gen(args) {
-    args.forEach(item => {
-        yield item + 1
-    })
+// function* gen(args) {
+//     args.forEach(item => {
+//         yield item + 1
+//     })
+// }
+
+function* gen(x) {
+    let y = 2 * (yield(x + 1))
+    let z = yield(y / 3)
+    return x + y + z
 }
+let g = gen(5)
+console.log(g.next()) // 6
+console.log(g.next()) // NaN
+console.log(g.next()) // NaN
