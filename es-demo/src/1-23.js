251c618ecffb992800e0ca52be6c27117115f28f
6xd <ldtheended@163.com>
2020-11-26 11:24:04 +0800

    a2.98（异步编程解决方案Promise——Promise状态不可逆）

--- es-demo/src/1-23.js
+++ es-demo/src/1-23.js
@@ -22,36 +22,46 @@
 //     console.log(3)
 // })
 
-let p1 = new Promise((resolve, reject) => {
+// let p1 = new Promise((resolve, reject) => {
+//     resolve(1)
+// })
+// let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(2)
+//     }, 1000)
+// })
+// let p3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject(3)
+//     }, 1000)
+// })
+// console.log(p1) // resolved
+// console.log(p2) // pending
+// console.log(p3) // pending
+//
+// setTimeout(() => {
+//     console.log(p2)
+// }, 2000)
+// setTimeout(() => {
+//     console.log(p3)
+// }, 2000)
+//
+// p1.then(res => {
+//     console.log(res)
+// })
+// p2.then(res => {
+//     console.log(res)
+// })
+// p3.catch(err => {
+//     console.log(err)
+// })
+
+let p = new Promise((resolve, reject) => {
+    reject(2)
     resolve(1)
 })
-let p2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(2)
-    }, 1000)
-})
-let p3 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject(3)
-    }, 1000)
-})
-console.log(p1) // resolved
-console.log(p2) // pending
-console.log(p3) // pending
-
-setTimeout(() => {
-    console.log(p2)
-}, 2000)
-setTimeout(() => {
-    console.log(p3)
-}, 2000)
-
-p1.then(res => {
+p.then(res => {
     console.log(res)
-})
-p2.then(res => {
-    console.log(res)
-})
-p3.catch(err => {
+}).catch(err => {
     console.log(err)
 })
