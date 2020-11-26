8b290c60a80bc0294e24f4ee165d81612a2e863f
6xd <ldtheended@163.com>
2020-11-26 13:36:56 +0800

    a3.06（Promise的静态方法——应用场景  =>  有时没有promise实例，也想调用then和catch方法，则需要Promise静态方法包裹。）

--- es-demo/src/1-24.js
+++ es-demo/src/1-24.js
@@ -4,8 +4,30 @@
 //     console.log(res)
 // })
 
-let p2 = Promise.reject('fail')
-console.log(p2)
-p2.catch(err => {
+// let p2 = Promise.reject('fail')
+// console.log(p2)
+// p2.catch(err => {
+//     console.log(err)
+// })
+
+function foo(flag) {
+    if (flag) {
+        return new Promise(resolve => {
+            // 异步操作
+            resolve('success')
+        })
+    } else {
+        // return 'fail'
+        return Promise.reject('fail')
+    }
+}
+
+foo(true).then(res => {
+    console.log(res)
+})
+
+foo(false).then(res => {
+    console.log(res)
+}, err => {
     console.log(err)
 })
