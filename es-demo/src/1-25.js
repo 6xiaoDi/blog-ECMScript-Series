52cdbcbc2facec63a6d4c05f75745205815c5fce
6xd <ldtheended@163.com>
2020-11-26 23:15:49 +0800

    a3.18（异步编程解决方案Generator——数7应用）

--- es-demo/src/1-25.js
+++ es-demo/src/1-25.js
@@ -16,17 +16,33 @@
 //     })
 // }
 
-function* gen(x) {
-    let y = 2 * (yield(x + 1))
-    let z = yield(y / 3)
-    return x + y + z
-}
+// function* gen(x) {
+//     let y = 2 * (yield(x + 1))
+//     let z = yield(y / 3)
+//     return x + y + z
+// }
 // let g = gen(5)
 // console.log(g.next()) // 6
 // console.log(g.next()) // NaN
 // console.log(g.next()) // NaN
 
-let g = gen(5)
-console.log(g.next()) // 6
-console.log(g.next(12)) // y=24  8
-console.log(g.next(13)) // z=13 x=5 42
+// let g = gen(5)
+// console.log(g.next()) // 6
+// console.log(g.next(12)) // y=24  8
+// console.log(g.next(13)) // z=13 x=5 42
+
+function* count(x = 1) {
+    while (true) {
+        if (x % 7 === 0) {
+            yield x
+        }
+        x++
+    }
+}
+let n = count()
+console.log(n.next().value)
+console.log(n.next().value)
+console.log(n.next().value)
+console.log(n.next().value)
+console.log(n.next().value)
+
