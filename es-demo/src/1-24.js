da0206c2089afb251d7abce082715311adf04ca3
6xd <ldtheended@163.com>
2020-11-26 17:44:36 +0800

    a3.11（Promise的静态方法——promise.all应用场景）

--- es-demo/src/1-24.js
+++ es-demo/src/1-24.js
@@ -32,25 +32,25 @@
 //     console.log(err)
 // })
 
-let p1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log(1)
-        resolve('1成功')
-    }, 2000)
-})
-let p2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log(2)
-        // resolve('2成功')
-        reject('2失败')
-    }, 1000)
-})
-let p3 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log(3)
-        resolve('3成功')
-    }, 3000)
-})
+// let p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log(1)
+//         resolve('1成功')
+//     }, 2000)
+// })
+// let p2 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log(2)
+//         // resolve('2成功')
+//         reject('2失败')
+//     }, 1000)
+// })
+// let p3 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log(3)
+//         resolve('3成功')
+//     }, 3000)
+// })
 
 // Promise.all([p1, p2, p3]).then(res => {
 //     console.log(res)
@@ -58,8 +58,21 @@ let p3 = new Promise((resolve, reject) => {
 //     console.log(err)
 // })
 
-Promise.race([p1, p2, p3]).then(res => {
-    console.log(res)
-}, err => {
-    console.log(err)
+// Promise.race([p1, p2, p3]).then(res => {
+//     console.log(res)
+// }, err => {
+//     console.log(err)
+// })
+
+const imgArr = ['1.jpg', '2.jpg', '3.jpg']
+let promiseArr = []
+imgArr.forEach(item => {
+    promiseArr.push(new Promise((resolve, reject) => {
+        // 图片上传的操作（省略）
+        resolve()
+    }))
+})
+Promise.all(promiseArr).then(res => {
+    // 插入数据库的操作
+    console.log('图片全部上传完成')
 })
