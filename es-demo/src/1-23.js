f8fe58060251ce981b76bb77c818fe7a80dbef63
6xd <ldtheended@163.com>
2020-11-26 10:14:23 +0800

    a2.95（异步编程解决方案Promise——笔试题1）

--- es-demo/src/1-23.js
+++ es-demo/src/1-23.js
@@ -1,14 +1,23 @@
 // 状态管理
 // resolve 成功
 // reject 失败
+// let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('123')
+//         // resolve('成功')
+//         reject('失败')
+//     }, 1000)
+// }).then(res => {
+//     console.log(res)
+// }, err => {
+//     console.log(err)
+// })
+
 let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('123')
-        // resolve('成功')
-        reject('失败')
-    }, 1000)
-}).then(res => {
-    console.log(res)
-}, err => {
-    console.log(err)
+    console.log(1)
+    resolve()
+})
+console.log(2)
+p.then(res => {
+    console.log(3)
 })
