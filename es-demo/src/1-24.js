a7f4ec8005be1d82f3d85ab7d257c88cf0d92d8c
6xd <ldtheended@163.com>
2020-11-26 13:26:22 +0800

    a3.05（Promise的静态方法——Promise.reject()）

--- es-demo/src/1-24.js
+++ es-demo/src/1-24.js
@@ -1,5 +1,11 @@
-let p1 = Promise.resolve('success')
-console.log(p1)
-p1.then(res => {
-    console.log(res)
+// let p1 = Promise.resolve('success')
+// console.log(p1)
+// p1.then(res => {
+//     console.log(res)
+// })
+
+let p2 = Promise.reject('fail')
+console.log(p2)
+p2.catch(err => {
+    console.log(err)
 })
