5ae467f578a2b79cd1a2f150eeea276f3938f2e0
6xd <ldtheended@163.com>
2020-11-26 23:23:24 +0800

    a3.19（异步编程解决方案Generator——实现ajax请求）

--- es-demo/src/1-25.js
+++ es-demo/src/1-25.js
@@ -31,18 +31,56 @@
 // console.log(g.next(12)) // y=24  8
 // console.log(g.next(13)) // z=13 x=5 42
 
-function* count(x = 1) {
-    while (true) {
-        if (x % 7 === 0) {
-            yield x
+// function* count(x = 1) {
+//     while (true) {
+//         if (x % 7 === 0) {
+//             yield x
+//         }
+//         x++
+//     }
+// }
+// let n = count()
+// console.log(n.next().value)
+// console.log(n.next().value)
+// console.log(n.next().value)
+// console.log(n.next().value)
+// console.log(n.next().value)
+
+function ajax(url, callback) {
+    // 1、创建XMLHttpRequest对象
+    var xmlhttp
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else { // 兼容早期浏览器
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+    // 2、发送请求
+    xmlhttp.open('GET', url, true)
+    xmlhttp.send()
+    // 3、服务端响应
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var obj = JSON.parse(xmlhttp.responseText)
+            // console.log(obj)
+            callback(obj)
         }
-        x++
     }
 }
-let n = count()
-console.log(n.next().value)
-console.log(n.next().value)
-console.log(n.next().value)
-console.log(n.next().value)
-console.log(n.next().value)
+
+function request(url) {
+    ajax(url, res => {
+        getData.next(res)
+    })
+}
+
+function* gen() {
+    let res1 = yield request('static/a.json')
+    console.log(res1)
+    let res2 = yield request('static/b.json')
+    console.log(res2)
+    let res3 = yield request('static/c.json')
+    console.log(res3)
+}
+let getData = gen()
+getData.next()
 
