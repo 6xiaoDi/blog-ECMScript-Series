55620ce298fb45c861e223166d2b346276337272
6xd <ldtheended@163.com>
2020-11-27 13:01:32 +0800

    a3.30（模块化Module——统一导出）

--- es-demo/src/module.js
+++ es-demo/src/module.js
@@ -1,7 +1,21 @@
-export const a = 5
-export const b = 'QACC'
-export const sum = (x, y) => x + y
+// export const a = 5
+// export const b = 'QACC'
+// export const sum = (x, y) => x + y
+// const obj = {
+//     name: 'es'
+// }
+// export {obj}
+
+const a = 5
+const b = 'QACC'
+const sum = (x, y) => x + y
 const obj = {
     name: 'es'
 }
-export {obj}
+
+export {
+    a,
+    b,
+    sum,
+    obj
+}
