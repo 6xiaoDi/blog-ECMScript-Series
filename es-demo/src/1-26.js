96fdecffe7e5a1ba3add9222619a02f095654c16
6xd <ldtheended@163.com>
2020-11-27 10:26:50 +0800

    a3.21（迭代器Iterator——对象遍历报错）

--- es-demo/src/1-26.js
+++ es-demo/src/1-26.js
@@ -1,19 +1,31 @@
-function makeIterator(arr) {
-    let nextIndex = 0
-    return {
-        next() {
-            return nextIndex < arr.length ? {
-                value: arr[nextIndex++],
-                done: false
-            } : {
-                value: undefined,
-                done: true
-            }
-        }
+// function makeIterator(arr) {
+//     let nextIndex = 0
+//     return {
+//         next() {
+//             return nextIndex < arr.length ? {
+//                 value: arr[nextIndex++],
+//                 done: false
+//             } : {
+//                 value: undefined,
+//                 done: true
+//             }
+//         }
+//     }
+// }
+// let it = makeIterator(['a', 'b', 'c'])
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+
+let courses = {
+    allCourse: {
+        frontend: ['ES', '小程序', 'Vue', 'React'],
+        backend: ['Java', 'Python', 'SpringBoot'],
+        webapp: ['Android', 'IOS']
     }
 }
-let it = makeIterator(['a', 'b', 'c'])
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
+
+for(let c of courses){
+    console.log(c)
+}
