13e00122e83be3f53276b0867ec7c95b673f0171
6xd <ldtheended@163.com>
2020-11-27 22:08:46 +0800

    a3.53（异步编程解决方案Async Await——发送ajax请求，使用async和await）

--- es-demo/src/3-1.js
+++ es-demo/src/3-1.js
@@ -24,18 +24,37 @@
 // }
 // foo()
 
-function timeout() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject('fail')
-        }, 1000)
+// function timeout() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject('fail')
+//         }, 1000)
+//     })
+// }
+// async function foo() {
+//     return await timeout()
+// }
+// foo().then(res => {
+//     console.log(res);
+// }).catch(err => {
+//     console.log(err)
+// })
+
+import ajax from './ajax'
+
+function request(url) {
+    return new Promise(resolve => {
+        ajax(url, res => {
+            resolve(res)
+        })
     })
 }
-async function foo() {
-    return await timeout()
+async function getData(){
+    const res1 = await request('static/a.json')
+    console.log(res1)
+    const res2 = await request('static/b.json')
+    console.log(res2)
+    const res3 = await request('static/c.json')
+    console.log(res3)
 }
-foo().then(res => {
-    console.log(res);
-}).catch(err => {
-    console.log(err)
-})
+getData()
