39a07d48ddd0886527119537c08f830dc600391d
6xd <ldtheended@163.com>
2020-11-27 12:57:05 +0800

    a3.29（模块化Module——导入导出函数和对象）

--- es-demo/src/1-27.js
+++ es-demo/src/1-27.js
@@ -1,2 +1,9 @@
-import {a,b} from './module'
+import {
+    a,
+    b,
+    sum,
+    obj
+} from './module'
 console.log(a,b)
+console.log(sum(2, 5))
+console.log(obj)
--- es-demo/src/module.js
+++ es-demo/src/module.js
@@ -1,2 +1,7 @@
 export const a = 5
 export const b = 'QACC'
+export const sum = (x, y) => x + y
+const obj = {
+    name: 'es'
+}
+export {obj}
