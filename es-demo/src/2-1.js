2c5fae027c43153b0fd104022b3e7f957b27b8ed
6xd <ldtheended@163.com>
2020-11-27 14:06:29 +0800

    a3.42（数组扩展：Array.prototype.includes()——es5中indexOf方法的使用）

--- es-demo/src/2-1.js
+++ es-demo/src/2-1.js
@@ -1,8 +1,11 @@
 // includes -> boolean
 const arr = ['es6', 'es7', 'es8']
-console.log(arr.includes('es7')) // true
-console.log(arr.includes('es9')) // false
-console.log(arr.includes('es7', 1)) // true
-console.log(arr.includes('es7', 2)) // false
-console.log(arr.includes('es7', -1)) // false
-console.log(arr.includes('es7', -2)) // true
+// console.log(arr.includes('es7')) // true
+// console.log(arr.includes('es9')) // false
+// console.log(arr.includes('es7', 1)) // true
+// console.log(arr.includes('es7', 2)) // false
+// console.log(arr.includes('es7', -1)) // false
+// console.log(arr.includes('es7', -2)) // true
+
+console.log(arr.indexOf('es7'))
+console.log(arr.indexOf('es7') > -1)
