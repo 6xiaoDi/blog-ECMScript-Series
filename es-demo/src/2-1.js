dfdbdc11d4521a1caf5daa0165b96102d9885fdd
6xd <ldtheended@163.com>
2020-11-27 14:09:35 +0800

    a3.43（数组扩展：Array.prototype.includes()——includes和indexOf都不适用判断非基本类型数据）

--- es-demo/src/2-1.js
+++ es-demo/src/2-1.js
@@ -1,5 +1,5 @@
 // includes -> boolean
-const arr = ['es6', 'es7', 'es8']
+// const arr = ['es6', 'es7', 'es8']
 // console.log(arr.includes('es7')) // true
 // console.log(arr.includes('es9')) // false
 // console.log(arr.includes('es7', 1)) // true
@@ -7,5 +7,9 @@ const arr = ['es6', 'es7', 'es8']
 // console.log(arr.includes('es7', -1)) // false
 // console.log(arr.includes('es7', -2)) // true
 
-console.log(arr.indexOf('es7'))
-console.log(arr.indexOf('es7') > -1)
+// console.log(arr.indexOf('es7'))
+// console.log(arr.indexOf('es7') > -1)
+
+const arr = ['es6', ['es7', 'es8'], 'es9']
+console.log(arr.includes(['es7', 'es8'])) // false
+console.log(arr.indexOf(['es7', 'es8'])) // -1
