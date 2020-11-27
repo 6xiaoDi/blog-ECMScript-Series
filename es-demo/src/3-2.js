1c6cd70dae1500b839cb7376d18e4781b252fe6f
6xd <ldtheended@163.com>
2020-11-27 23:25:05 +0800

    a3.56（对象扩展：Object.values()，Object.entries()——Object.entries传数组）

--- es-demo/src/3-2.js
+++ es-demo/src/3-2.js
@@ -1,14 +1,16 @@
-const obj = {
-    name: 'baidu',
-    web: 'www.baidu.com',
-    target: 'es'
-}
+// const obj = {
+//     name: 'baidu',
+//     web: 'www.baidu.com',
+//     target: 'es'
+// }
 // console.log(Object.keys(obj))
 // const res = Object.keys(obj).map(key => obj[key])
 // console.log(res)
 
-console.log(Object.values(obj))
-console.log(Object.entries(obj))
-for(let [key, val] of Object.entries(obj)){
-    console.log(`${key}: ${val}`)
-}
+// console.log(Object.values(obj))
+// console.log(Object.entries(obj))
+// for(let [key, val] of Object.entries(obj)){
+//     console.log(`${key}: ${val}`)
+// }
+
+console.log(Object.entries(['a', 'b', 'c']))
