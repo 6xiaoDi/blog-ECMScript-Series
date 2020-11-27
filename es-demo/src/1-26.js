0b1c7928538384b53387bd13500692f8002b2380
6xd <ldtheended@163.com>
2020-11-27 11:49:44 +0800

    a3.24（迭代器Iterator——自定义结构可遍历）

--- es-demo/src/1-26.js
+++ es-demo/src/1-26.js
@@ -18,14 +18,6 @@
 // console.log(it.next())
 // console.log(it.next())
 
-let courses = {
-    allCourse: {
-        frontend: ['ES', '小程序', 'Vue', 'React'],
-        backend: ['Java', 'Python', 'SpringBoot'],
-        webapp: ['Android', 'IOS']
-    }
-}
-
 // for(let c of courses){
 //     console.log(c)
 // }
@@ -38,14 +30,45 @@ let courses = {
 // console.log(it.next())
 // console.log(it.next())
 
-let map = new Map()
-map.set('name', 'es')
-map.set('age', 5)
-map.set('school', 'QH')
-console.log(map)
-let it = map[Symbol.iterator]()
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
+// let map = new Map()
+// map.set('name', 'es')
+// map.set('age', 5)
+// map.set('school', 'QH')
+// console.log(map)
+// let it = map[Symbol.iterator]()
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
 
+let courses = {
+    allCourse: {
+        frontend: ['ES', '小程序', 'Vue', 'React'],
+        backend: ['Java', 'Python', 'SpringBoot'],
+        webapp: ['Android', 'IOS']
+    }
+}
+
+courses[Symbol.iterator] = function () {
+    let allCourse = this.allCourse
+    let keys = Reflect.ownKeys(allCourse)
+    let values = []
+    return {
+        next() {
+            if (!values.length) {
+                if (keys.length) {
+                    values = allCourse[keys[0]]
+                    keys.shift()
+                }
+            }
+            return {
+                done: !values.length,
+                value: values.shift()
+            }
+        }
+    }
+}
+
+for (let c of courses) {
+    console.log(c)
+}
