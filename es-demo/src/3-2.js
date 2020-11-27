f477b9c1b1a667071a221ff69516f2d8ffb651ea
6xd <ldtheended@163.com>
2020-11-27 23:22:12 +0800

    a3.55（对象扩展：Object.values()，Object.entries()——基本使用）

--- es-demo/src/3-2.js
+++ es-demo/src/3-2.js
@@ -3,6 +3,12 @@ const obj = {
     web: 'www.baidu.com',
     target: 'es'
 }
-console.log(Object.keys(obj))
-const res = Object.keys(obj).map(key => obj[key])
-console.log(res)
+// console.log(Object.keys(obj))
+// const res = Object.keys(obj).map(key => obj[key])
+// console.log(res)
+
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+for(let [key, val] of Object.entries(obj)){
+    console.log(`${key}: ${val}`)
+}
