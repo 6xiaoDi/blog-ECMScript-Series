329f7f2787a8c62c83069d97038b4745e0d6e30a
6xd <ldtheended@163.com>
2020-11-28 14:03:48 +0800

    a3.73（正则表达式扩展——匹配年月日的传统写法）

--- es-demo/src/4-2.js
+++ es-demo/src/4-2.js
@@ -8,10 +8,17 @@
 // console.log(reg.test('\u{2028}')) // false
 // console.log(reg.test('\u{2029}')) // false
 
-const reg = /./s
-console.log(reg.test('5')) // true
-console.log(reg.test('x')) // true
-console.log(reg.test('\n')) // true
-console.log(reg.test('\r')) // true
-console.log(reg.test('\u{2028}')) // true
-console.log(reg.test('\u{2029}')) // true
+// const reg = /./s
+// console.log(reg.test('5')) // true
+// console.log(reg.test('x')) // true
+// console.log(reg.test('\n')) // true
+// console.log(reg.test('\r')) // true
+// console.log(reg.test('\u{2028}')) // true
+// console.log(reg.test('\u{2029}')) // true
+
+// 具名组匹配
+const date = /(\d{4})-(\d{2})-(\d{2})/.exec('2020-01-01')
+console.log(date)
+console.log(date[1])  // 年
+console.log(date[2])  // 月
+console.log(date[3])  // 日
