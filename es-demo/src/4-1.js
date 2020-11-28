0807f737f7be16d2bca33b6c6949e0826ac2e6d1
6xd <ldtheended@163.com>
2020-11-28 13:26:57 +0800

    a3.70（异步迭代：for await of——基本使用）

--- es-demo/src/4-1.js
+++ es-demo/src/4-1.js
@@ -20,14 +20,28 @@
 function getPromise(time) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(time)
+            resolve({
+                value: time,
+                done: false
+            })
         }, time)
     })
 }
 const arr = [getPromise(1000), getPromise(2000), getPromise(3000)]
-
+arr[Symbol.asyncIterator] = function () {
+    let nextIndex = 0
+    return {
+        next() {
+            return nextIndex < arr.length ? arr[nextIndex++] :
+                Promise.resolve({
+                    value: undefined,
+                    done: true
+                })
+        }
+    }
+}
 async function test() {
-    for (let item of arr) {
+    for await (let item of arr) {
         console.log(item)
     }
 }
