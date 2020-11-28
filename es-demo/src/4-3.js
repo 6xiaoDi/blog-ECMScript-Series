ed87b68516071097abd4a3bde29efe6ba7432f2f
6xd <ldtheended@163.com>
2020-11-28 21:37:22 +0800

    a3.84（对象扩展：Rest & Spread——对象剩余运算符）

--- es-demo/src/4-3.js
+++ es-demo/src/4-3.js
@@ -3,14 +3,14 @@
 // const arr3 = [...arr1, ...arr2]
 // console.log(arr3)
 
-const obj1 = {
-    name: 'lisi',
-    age: 24
-}
-const obj2 = {
-    school: 'QH',
-    age: 18
-}
+// const obj1 = {
+//     name: 'lisi',
+//     age: 24
+// }
+// const obj2 = {
+//     school: 'QH',
+//     age: 18
+// }
 
 // 克隆对象
 // const obj3 = {
@@ -20,5 +20,17 @@ const obj2 = {
 // console.log(obj3)
 
 // 合并对象
-const obj4 = {...obj1, ...obj2}
-console.log(obj4)
+// const obj4 = {...obj1, ...obj2}
+// console.log(obj4)
+
+const obj1 = {
+    name: 'lisi',
+    age: 24,
+    school: 'QH',
+    course: 'es'
+}
+
+const {name, age, ...rest} = obj1
+console.log(name)
+console.log(age)
+console.log(rest)
