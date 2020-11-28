279b43cf2dd6404f9314dfe76666b312073ac37b
6xd <ldtheended@163.com>
2020-11-28 21:20:14 +0800

    a3.82（对象扩展：Rest & Spread——利用扩展运算符克隆对象）

--- es-demo/src/4-3.js
+++ es-demo/src/4-3.js
@@ -1,4 +1,16 @@
-const arr1 = [1, 2, 3]
-const arr2 = [4, 5, 6]
-const arr3 = [...arr1, ...arr2]
-console.log(arr3)
+// const arr1 = [1, 2, 3]
+// const arr2 = [4, 5, 6]
+// const arr3 = [...arr1, ...arr2]
+// console.log(arr3)
+
+const obj1 = {
+    name: 'lisi',
+    age: 24
+}
+
+// 克隆对象
+const obj3 = {
+    ...obj1
+}
+obj1.age = 18
+console.log(obj3)
