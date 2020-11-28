b056d3c85d746ad1c6476f96a4ba6a2abe625289
6xd <ldtheended@163.com>
2020-11-28 13:01:48 +0800

    a3.69（异步迭代：for await of——异步操作遍历）

--- es-demo/src/4-1.js
+++ es-demo/src/4-1.js
@@ -1,18 +1,34 @@
-const arr = ['es6', 'es7', 'es8', 'es9']
-arr[Symbol.iterator] = function () {
-    let nextIndex = 0
-    return {
-        next() {
-            return nextIndex < arr.length ? {
-                value: arr[nextIndex++],
-                done: false
-            } : {
-                value: undefined,
-                done: true
-            }
-        }
+// const arr = ['es6', 'es7', 'es8', 'es9']
+// arr[Symbol.iterator] = function () {
+//     let nextIndex = 0
+//     return {
+//         next() {
+//             return nextIndex < arr.length ? {
+//                 value: arr[nextIndex++],
+//                 done: false
+//             } : {
+//                 value: undefined,
+//                 done: true
+//             }
+//         }
+//     }
+// }
+// for (let item of arr) {
+//     console.log(item)
+// }
+
+function getPromise(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(time)
+        }, time)
+    })
+}
+const arr = [getPromise(1000), getPromise(2000), getPromise(3000)]
+
+async function test() {
+    for (let item of arr) {
+        console.log(item)
     }
 }
-for (let item of arr) {
-    console.log(item)
-}
+test()
