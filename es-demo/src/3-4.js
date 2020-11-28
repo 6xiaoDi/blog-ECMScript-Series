b7e22d729c2d556750f16e8e7b1f3e72dd3fb7a6
6xd <ldtheended@163.com>
2020-11-28 12:06:31 +0800

    a3.65（字符串扩展：String.prototype.padStart()，String.prototype.padEnd()——应用场景-拼接日期）

--- es-demo/src/3-4.js
+++ es-demo/src/3-4.js
@@ -1,4 +1,12 @@
-const str = 'baidu'
-console.log(str.padStart(8, 'x'))
-console.log(str.padEnd(8, 'y'))
-console.log(str.padStart(8))
+// const str = 'baidu'
+// console.log(str.padStart(8, 'x'))
+// console.log(str.padEnd(8, 'y'))
+// console.log(str.padStart(8))
+
+// yyyy-mm-dd  2020-11-01
+const now = new Date()
+const year = now.getFullYear()
+const month = (now.getMonth() + 1).toString().padStart(2, '0') // 0~11
+const day = (now.getDate()).toString().padStart(2, '0')
+console.log(`${year}-${month}-${day}`)
+
