40d6c1b8e6b83f79d1cd2b38e0294184d3524d7c
6xd <ldtheended@163.com>
2020-11-28 10:56:16 +0800

    a3.58（对象属性描述： Object.getOwnPropertyDescriptors()——writable 为false，则不可写。）

--- es-demo/src/3-3.js
+++ es-demo/src/3-3.js
@@ -1,7 +1,20 @@
-const obj = {
-    name: 'baidu',
-    target: 'es'
-}
+// const obj = {
+//     name: 'baidu',
+//     target: 'es'
+// }
+//
+// const desc = Object.getOwnPropertyDescriptors(obj)
+// console.log(desc)
 
-const desc = Object.getOwnPropertyDescriptors(obj)
-console.log(desc)
+const obj = {}
+Reflect.defineProperty(obj, 'name', {
+    value: 'lisi',
+    writable: false,
+    configurable: false,
+    enumerable: false
+})
+
+console.log(obj)
+obj.name = 'zhangsan'
+console.log(obj)
+
