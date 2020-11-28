b763495497a4e9eda361cf219036ce9014bcb395
6xd <ldtheended@163.com>
2020-11-28 13:57:03 +0800

    a3.72（正则表达式扩展——dotAll  => ` /./s`）

--- es-demo/src/4-2.js
+++ es-demo/src/4-2.js
@@ -1,9 +1,17 @@
 // dot  www.baidu.com  dotAll
 // .
-const reg = /./
+// const reg = /./
+// console.log(reg.test('5')) // true
+// console.log(reg.test('x')) // true
+// console.log(reg.test('\n')) // false
+// console.log(reg.test('\r')) // false
+// console.log(reg.test('\u{2028}')) // false
+// console.log(reg.test('\u{2029}')) // false
+
+const reg = /./s
 console.log(reg.test('5')) // true
 console.log(reg.test('x')) // true
-console.log(reg.test('\n')) // false
-console.log(reg.test('\r')) // false
-console.log(reg.test('\u{2028}')) // false
-console.log(reg.test('\u{2029}')) // false
+console.log(reg.test('\n')) // true
+console.log(reg.test('\r')) // true
+console.log(reg.test('\u{2028}')) // true
+console.log(reg.test('\u{2029}')) // true
