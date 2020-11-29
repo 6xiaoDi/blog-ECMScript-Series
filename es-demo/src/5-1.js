91f836638ee745195bdf00eb463ca4ce56ff8b61
6xd <ldtheended@163.com>
2020-11-29 14:43:54 +0800

    a3.94（对象扩展：Object.fromEntries()——可将map转为键值对的对象）

--- es-demo/src/5-1.js
+++ es-demo/src/5-1.js
@@ -1,12 +1,20 @@
-const obj = {
-    name: 'QH',
-    course: 'es'
-}
-const entries = Object.entries(obj)
-console.log(entries)
+// const obj = {
+//     name: 'QH',
+//     course: 'es'
+// }
+// const entries = Object.entries(obj)
+// console.log(entries)
 
 // ES10
-const fromEntries = Object.fromEntries(entries)
+// const fromEntries = Object.fromEntries(entries)
+// console.log(fromEntries)
+
+// map -> 对象
+const map = new Map()
+map.set('name', 'QH')
+map.set('course', 'es')
+console.log(map)
+const fromEntries = Object.fromEntries(map)
 console.log(fromEntries)
 
 
