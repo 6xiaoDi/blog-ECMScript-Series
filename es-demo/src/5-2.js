ac7d7f30d9c38f9555eeb1d4ef07131d8a03846d
6xd <ldtheended@163.com>
2020-11-29 16:37:39 +0800

    a3.98（字符串扩展——String.prototype.trimStart() ，String.prototype.trimEnd()基本使用）

--- es-demo/src/5-2.js
+++ es-demo/src/5-2.js
@@ -1,6 +1,15 @@
 const str = '   abcde   '
 // 正则
-console.log(str)
-console.log(str.replace(/^\s+/g, '')) // 去掉前面的空格
-console.log(str.replace(/\s+$/g, '')) // 去掉后面的空格
-console.log(str.replace(/^\s+/g, '').replace(/\s+$/g, ''))
+// console.log(str)
+// console.log(str.replace(/^\s+/g, '')) // 去掉前面的空格
+// console.log(str.replace(/\s+$/g, '')) // 去掉后面的空格
+// console.log(str.replace(/^\s+/g, '').replace(/\s+$/g, ''))
+
+// 去掉前面的空格（两种写法）
+console.log(str.trimStart())
+console.log(str.trimLeft())
+// 去掉后面的空格（两种写法）
+console.log(str.trimEnd())
+console.log(str.trimRight())
+// 去除前后空格
+console.log(str.trim())
