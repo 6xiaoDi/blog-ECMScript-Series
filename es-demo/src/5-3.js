714c9ce105001e9902b937b912dc296900ab43b0
6xd <ldtheended@163.com>
2020-11-29 17:18:01 +0800

    a4.01（数组扩展——Array.prototype.map()基本使用）

--- es-demo/src/5-3.js
+++ es-demo/src/5-3.js
@@ -1,6 +1,10 @@
-const arr = [1, 2, 3, [4, 5, 6, [7, 8, 9, [10, 11, 12]]]]
-console.log(arr.flat())
-console.log(arr.flat().flat())
-console.log(arr.flat().flat().flat())
-console.log(arr.flat(3))
-console.log(arr.flat(Infinity))
+// const arr = [1, 2, 3, [4, 5, 6, [7, 8, 9, [10, 11, 12]]]]
+// console.log(arr.flat())
+// console.log(arr.flat().flat())
+// console.log(arr.flat().flat().flat())
+// console.log(arr.flat(3))
+// console.log(arr.flat(Infinity))
+
+const arr = [1, 2, 3, 4, 5]
+const res = arr.map(x => x + 1)
+console.log(res)
