6dac5f132d3371cea6a089ca9874959f22dbf7c5
6xd <ldtheended@163.com>
2020-11-29 14:53:11 +0800

    a3.95（对象扩展：Object.fromEntries()——应用）

--- es-demo/src/5-1.js
+++ es-demo/src/5-1.js
@@ -10,11 +10,19 @@
 // console.log(fromEntries)
 
 // map -> 对象
-const map = new Map()
-map.set('name', 'QH')
-map.set('course', 'es')
-console.log(map)
-const fromEntries = Object.fromEntries(map)
-console.log(fromEntries)
+// const map = new Map()
+// map.set('name', 'QH')
+// map.set('course', 'es')
+// console.log(map)
+// const fromEntries = Object.fromEntries(map)
+// console.log(fromEntries)
 
+const course = {
+    math: 80,
+    english: 85,
+    chinese: 90
+}
+const res = Object.entries(course).filter(([key, val]) => val > 80)
+console.log(res)
+console.log(Object.fromEntries(res))
 
