30ae9b083bfe54b092df83b757e8cad297bb9d3f
6xd <ldtheended@163.com>
2020-11-30 10:40:14 +0800

    a4.09（JSON扩展——JSON.stringify()增强能力）

--- es-demo/src/5-6.js
+++ es-demo/src/5-6.js
@@ -1,3 +1,7 @@
 // JSON 超集 \u2029 \u2028
-eval('var str="abcdef";\u2029 function foo(){return str;}')
-console.log(foo())
+// eval('var str="abcdef";\u2029 function foo(){return str;}')
+// console.log(foo())
+
+// 0xD800~0xDfff
+console.log(JSON.stringify('\uD83D\uDE0E')) // emoji
+console.log(JSON.stringify('\uD83D'))
