c56b6c616ea1a42a196c9513f3bcf1a4a62c4028
6xd <ldtheended@163.com>
2020-11-30 15:34:51 +0800

    a4.22（新的原始数据类型：BigInt——与普通数相比，同样的数值相等但类型不等）

--- es-demo/src/6-3.js
+++ es-demo/src/6-3.js
@@ -5,7 +5,9 @@
 
 // console.log(max === max + 1) // true
 
-const bigInt = 9007199254740993n
-console.log(bigInt)
-console.log(typeof bigInt) // bigint
+// const bigInt = 9007199254740993n
+// console.log(bigInt)
+// console.log(typeof bigInt) // bigint
 
+console.log(1n == 1) // true
+console.log(1n === 1) // false
