5a5f041fb5c7c72026588410aae9d50cdabf7d96
6xd <ldtheended@163.com>
2020-11-30 15:39:03 +0800

    a4.23（新的原始数据类型：BigInt——特性）

--- es-demo/src/6-3.js
+++ es-demo/src/6-3.js
@@ -5,9 +5,15 @@
 
 // console.log(max === max + 1) // true
 
-// const bigInt = 9007199254740993n
+const bigInt = 9007199254740993n
 // console.log(bigInt)
 // console.log(typeof bigInt) // bigint
 
-console.log(1n == 1) // true
-console.log(1n === 1) // false
+// console.log(1n == 1) // true
+// console.log(1n === 1) // false
+
+const bigInt2 = BigInt(9007199254740993n)
+console.log(bigInt2)
+
+const num = bigInt + bigInt2
+console.log(num.toString())
