36496459f13948c491030758b79a93d554313e13
6xd <ldtheended@163.com>
2020-11-30 15:32:40 +0800

    a4.21（新的原始数据类型：BigInt——基本使用）

--- es-demo/src/6-3.js
+++ es-demo/src/6-3.js
@@ -1,6 +1,11 @@
-const max = 2 ** 53
-console.log(max)
+// const max = 2 ** 53
+// console.log(max)
 
-console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MAX_SAFE_INTEGER)
 
-console.log(max === max + 1) // true
+// console.log(max === max + 1) // true
+
+const bigInt = 9007199254740993n
+console.log(bigInt)
+console.log(typeof bigInt) // bigint
+
