2dc9acff98520f20794c7b404b3b3c2f2bf9de8a
6xd <ldtheended@163.com>
2020-11-30 16:04:05 +0800

    a4.26（Promise扩展——Promise.allSettled()基本使用）

--- es-demo/src/6-4.js
+++ es-demo/src/6-4.js
@@ -1,4 +1,27 @@
-Promise.all([
+// Promise.all([
+//     Promise.resolve({
+//         code: 200,
+//         data: [1, 2, 3]
+//     }),
+//     Promise.reject({
+//         code: 500,
+//         data: []
+//     }),
+//     Promise.resolve({
+//         code: 200,
+//         data: [7, 8, 9]
+//     }),
+// ]).then(res=>{
+//     console.log(res)
+//     console.log('成功')
+// }).catch(err=>{
+//     console.log(err)
+//     console.log('失败')
+// })
+
+
+// es2020
+Promise.allSettled([
     Promise.resolve({
         code: 200,
         data: [1, 2, 3]
