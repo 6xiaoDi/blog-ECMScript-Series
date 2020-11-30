d6a25c75956c92eff8f035ebf76b0d9c968f3473
6xd <ldtheended@163.com>
2020-11-30 11:18:39 +0800

    a4.11（Symbol扩展——Symbol.prototype.description特征）

--- es-demo/src/5-7.js
+++ es-demo/src/5-7.js
@@ -3,3 +3,8 @@ const s = Symbol('ABCDEF')
 console.log(s)
 console.log(s.description) // es10新增只读属性
 
+s.description = 'es'
+console.log(s)
+
+const s2 = Symbol()
+console.log(s2.description) // undefined
