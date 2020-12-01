311e19dc3f782df655f2ad4788578926f3df1fb3
6xd <ldtheended@163.com>
2020-12-01 09:25:01 +0800

    a4.28（全局对象：globalThis——self）

---
+++ es-demo/src/6-5.js
@@ -0,0 +1,3 @@
+self.setTimeout(()=>{
+    console.log('es2020')
+}, 1000)
--- es-demo/src/index.js
+++ es-demo/src/index.js
@@ -49,4 +49,5 @@
 // import './6-1'
 // import './6-2'
 // import './6-3'
-import './6-4'
+// import './6-4'
+import './6-5'
