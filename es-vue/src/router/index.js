37f8f8b5cc77b236788467be0dcf3e02aa6bca3d
6xd <ldtheended@163.com>
2020-12-01 14:30:11 +0800

    a4.38（引入用户页面）

--- es-vue/src/router/index.js
+++ es-vue/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import User from '../views/User.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'User',
+    component: User
   },
   {
     path: '/about',
