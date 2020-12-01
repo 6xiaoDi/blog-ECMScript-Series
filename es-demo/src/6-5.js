adb122db9bc68371e8e097897eb631dfa34b7d60
6xd <ldtheended@163.com>
2020-12-01 09:29:51 +0800

    a4.29（全局对象：globalThis——在之前环境中，想要获取任何环境下的全局变量则需要判断）

--- es-demo/src/6-5.js
+++ es-demo/src/6-5.js
@@ -1,3 +1,19 @@
-self.setTimeout(()=>{
-    console.log('es2020')
-}, 1000)
+// self.setTimeout(()=>{
+//     console.log('es2020')
+// }, 1000)
+
+const getGlobal = () => {
+    if (typeof self !== 'undefined') {
+        return self
+    }
+    if (typeof window !== 'undefined') {
+        return window
+    }
+    if (typeof global !== 'undefined') {
+        return global
+    }
+    throw new Error('无法找到全局对象')
+}
+const global = getGlobal()
+console.log(global)
+
