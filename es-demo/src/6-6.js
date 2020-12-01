7b2696426aae6ab4953c07b4971d9072349517ba
6xd <ldtheended@163.com>
2020-12-01 09:42:27 +0800

    a4.31（可选链：Optional chaining——之前判断方式）

---
+++ es-demo/src/6-6.js
@@ -0,0 +1,14 @@
+// 可选链
+const user = {
+    address: {
+        street: 'xx街道',
+        getNum() {
+            return '80号'
+        }
+    }
+}
+const street = user && user.address && user.address.street
+console.log(street)
+
+const num = user && user.address && user.address.getNum && user.address.getNum()
+console.log(num)
--- es-demo/src/index.js
+++ es-demo/src/index.js
@@ -50,4 +50,5 @@
 // import './6-2'
 // import './6-3'
 // import './6-4'
-import './6-5'
+// import './6-5'
+import './6-6'
