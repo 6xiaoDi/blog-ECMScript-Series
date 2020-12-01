ea9cbfe401a8548ff45980a77425f81f8e3c0e77
6xd <ldtheended@163.com>
2020-12-01 09:34:46 +0800

    a4.30（全局对象：globalThis——基本使用）

--- es-demo/src/6-5.js
+++ es-demo/src/6-5.js
@@ -2,18 +2,19 @@
 //     console.log('es2020')
 // }, 1000)
 
-const getGlobal = () => {
-    if (typeof self !== 'undefined') {
-        return self
-    }
-    if (typeof window !== 'undefined') {
-        return window
-    }
-    if (typeof global !== 'undefined') {
-        return global
-    }
-    throw new Error('无法找到全局对象')
-}
-const global = getGlobal()
-console.log(global)
+// const getGlobal = () => {
+//     if (typeof self !== 'undefined') {
+//         return self
+//     }
+//     if (typeof window !== 'undefined') {
+//         return window
+//     }
+//     if (typeof global !== 'undefined') {
+//         return global
+//     }
+//     throw new Error('无法找到全局对象')
+// }
+// const global = getGlobal()
+// console.log(global)
 
+console.log(globalThis)
