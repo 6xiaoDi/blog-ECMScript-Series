945b44dd11a468e38ee4f85707a46fe3f97e79da
6xd <ldtheended@163.com>
2020-12-01 17:52:41 +0800

    a4.39（获取用户数据）

--- es-vue/src/views/User.vue
+++ es-vue/src/views/User.vue
@@ -1,13 +1,39 @@
 <template>
+  <div>
     <div>用户信息</div>
+    <ul>
+      <li v-for="(item, index) in userList" :key="index">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "User"
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      userList: []
     }
+  },
+  created () {
+    axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res)
+        this.userList = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 </script>
 
 <style scoped>
-
+li {
+  list-style: none;
+}
+ul {
+  padding: 0;
+}
 </style>
