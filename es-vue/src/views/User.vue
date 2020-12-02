b234b268850023bcbbfd9fcf0f36047b07541e39
6xd <ldtheended@163.com>
2020-12-02 12:45:57 +0800

    a4.41（使用Proxy代理实现用户信息排序）

--- es-vue/src/views/User.vue
+++ es-vue/src/views/User.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div>用户信息</div>
+    <button @click="asc">升序</button>
+    <button @click="desc">降序</button>
+    <button @click="reset">重置</button>
     <ul>
       <li v-for="(item, index) in userList" :key="index">{{ item.name }}</li>
     </ul>
@@ -18,7 +21,35 @@ export default {
   async created () {
     // 解构赋值
     const { data } = await axios.get('http://jsonplaceholder.typicode.com/users')
-    this.userList = data
+    // 代理
+    this.proxy = new Proxy({}, {
+      get (target, key) {
+        if (key === 'asc') { // 升序
+          return [].concat(data).sort((a, b) => a.name > b.name ? 1 : -1)
+        } else if (key === 'desc') { // 降序
+          return [].concat(data).sort((a, b) => b.name > a.name ? 1 : -1)
+        } else {
+          return data
+        }
+      },
+      set () {
+        // 不允许设置
+        return false
+      }
+    })
+    // 代理默认值
+    this.userList = this.proxy.default
+  },
+  methods: {
+    asc () {
+      this.userList = this.proxy.asc
+    },
+    desc () {
+      this.userList = this.proxy.desc
+    },
+    reset () {
+      this.userList = this.proxy.default
+    }
   }
 }
 </script>
